e681e5eaaa67487c2e2098dc9124e13d0432a9ea
Nullable <admin@nullable.se>
2012-04-26 00:23:35 +0200

    Removed Authenticator, modified C<->S messages

--- src/main/scala/ClientMessages.scala
+++ src/main/scala/ClientMessages.scala
@@ -1,6 +1,5 @@
 package blocklandglass.messages.clients
 
-import blocklandglass.Authenticator
 import blocklandglass.messages._
 
 sealed trait S2CMessage extends Message
@@ -9,28 +8,24 @@ sealed trait C2SMessage extends Message
 case class HandshakeInit(name: String, blid: String) extends C2SMessage {
 	def serialize = Seq("handshake", "init", name, blid)
 }
-case class HandshakeChallenge(challenge: String) extends S2CMessage {
-	def serialize = Seq("handshake", "challenge", challenge)
-}
-case class HandshakeResponse(response: String) extends C2SMessage {
-	def serialize = Seq("handshake", "response", response)
-}
 case class HandshakeResult(accepted: Boolean) extends S2CMessage {
-	def serialize = Seq("handshake", "result", if(Authenticator.queryAuth()) { if (accepted) "1" else "0" } else "-1")
+	def serialize = Seq("handshake", "result", if (accepted) "1" else "0")
+}
+case object HandshakeUnregisteredResult extends S2CMessage {
+	def serialize = Seq("handshake", "notregistered")
 }
 
 object C2SMessageReader extends MessageReader[C2SMessage] {
 	protected def parse(message: Seq[String]) = message match {
 		case Seq("handshake", "init", name, blid) => Some(HandshakeInit(name, blid))
-		case Seq("handshake", "response", response) => Some(HandshakeResponse(response))
 		case _ => None
 	}
 }
 
 object S2CMessageReader extends MessageReader[S2CMessage] {
 	protected def parse(message: Seq[String]) = message match {
-		case Seq("handshake", "challenge", challenge) => Some(HandshakeChallenge(challenge))
 		case Seq("handshake", "result", result) => Some(HandshakeResult(result == "1"))
+		case Seq("handshake", "notregistered") => Some(HandshakeUnregisteredResult)
 		case _ => None
 	}
 }
