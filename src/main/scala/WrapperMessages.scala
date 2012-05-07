95fa49a89d5bf230b769fc65e48a497bef591817
Nullable <admin@nullable.se>
2012-05-07 20:57:23 +0200

    Wrapped the HandshakeInit key in a RSAPublicKeySpec

--- src/main/scala/WrapperMessages.scala
+++ src/main/scala/WrapperMessages.scala
@@ -1,12 +1,15 @@
 package blocklandglass.messages.wrappers
 
+import java.math.BigInteger
+import java.security.spec.RSAPublicKeySpec
+
 import blocklandglass.messages._
 
 sealed trait S2WMessage extends Message
 sealed trait W2SMessage extends Message
 
-case class HandshakeInit(keyModulo: BigInt, keyPubExp: BigInt) extends W2SMessage {
-	def serialize = Seq("handshake", "init", keyModulo.toString(10), keyPubExp.toString(10))
+case class HandshakeInit(pubkey: RSAPublicKeySpec) extends W2SMessage {
+	def serialize = Seq("handshake", "init", pubkey.getModulus().toString(10), pubkey.getPublicExponent().toString(10))
 }
 case class HandshakeChallenge(challenge: String) extends S2WMessage {
 	def serialize = Seq("handshake", "challenge", challenge)
@@ -20,7 +23,7 @@ case class HandshakeResult(accepted: Boolean) extends S2WMessage {
 
 object W2SMessageReader extends MessageReader[W2SMessage] {
 	protected def parse(message: Seq[String]) = message match {
-		case Seq("handshake", "init", keyModulo, keyPubExp) => Some(HandshakeInit(BigInt(keyModulo, 10), BigInt(keyPubExp, 10)))
+		case Seq("handshake", "init", keyModulo, keyPubExp) => Some(HandshakeInit(new RSAPublicKeySpec(new BigInteger(keyModulo, 10), new BigInteger(keyPubExp, 10))))
 		case Seq("handshake", "response", response) => Some(HandshakeResponse(response))
 		case _ => None
 	}
