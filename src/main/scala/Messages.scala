e29c9f959c70d11dca45c46c9e9eae9d1b25776b
Nullable <admin@nullable.se>
2012-06-28 03:13:18 +0200

    Added ParsedMessage(message), moved processMessages to Glass-Common, bounded processMessages, ParsedMessage and MessageReader to Message

--- src/main/scala/Messages.scala
+++ src/main/scala/Messages.scala
@@ -11,7 +11,17 @@ trait Message {
 	def serializeToByteString: ByteString = ByteString(serializeToString, "UTF-8")
 }
 
-trait MessageReader[T] {
+object Message {
+	def processMessages[T <: Message](actor: ActorRef, reader: messages.MessageReader[T]) = IO repeat {
+		for {
+			message <- reader.read
+		} yield (actor ! ParsedMessage(message))
+	}
+}
+
+case class ParsedMessage[T <: Message](message: Option[T])
+
+trait MessageReader[T <: Message] {
 	def read: IO.Iteratee[Option[T]] = for {
 		message <- IO takeUntil ByteString("\r\n", "UTF-8")
 	} yield parse(message.decodeString("UTF-8").split("\t"))
